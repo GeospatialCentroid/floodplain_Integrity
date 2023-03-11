e0949f34275efb0430a94db0452cf7be238b832e
dcarver1 <carverd@colostate.edu>
2023-03-10 17:21:37 -0700

    revamped data processing input, near final output generation

--- 3_processValidationData.R
+++ 3_processValidationData.R
@@ -4,16 +4,26 @@
 # carverd@colostate.edu
 ###
 
-pacman::p_load("terra", "dplyr", "sf", "tmap")
+pacman::p_load("terra", "dplyr", "sf", "tmap", "raster")
 
 # read in data  -----------------------------------------------------------
+templateCRS <- raster("data/Konrad Ecological Functions/ecological_functions/fn1/w001000.adf")
+# EPSG:6339
 ## IFI flood plains 
-ifiFloodPlain <- sf::read_sf("data/CONUS_IFI_/CONUS_IFI_.shp")
-coreCRS <- crs(ifiFloodPlain)
+ifiFloodPlain <- sf::read_sf("data/CONUS_IFI_/CONUS_IFI_.shp")%>%
+  st_transform(crs = st_crs(templateCRS))
+
+ifi_fpCrop <- ifiFloodPlain %>% sf::st_crop(templateCRS) 
+sf::st_write(obj = ifi_fpCrop, dsn = "data/preppedValidataionData/ifi_fpCrop.geojson", delete_dsn = TRUE)
 
 ## IFI results
 ifiResults <- sf::read_sf("data/ifi/FP_stats_huc12.shp")%>%
-  sf::st_transform(crs = coreCRS)
+  sf::st_transform(st_crs(ifiFloodPlain))
+
+ifi_rCrop <- ifiResults %>% sf::st_crop(ext(templateCRS))
+sf::st_write(obj = ifi_rCrop, dsn = "data/preppedValidataionData/ifiResults.geojson", delete_dsn = TRUE)
+
+
 
 ## Konrad FN1 objects - rasters
 ### list all features
@@ -21,78 +31,26 @@ kfiles <- list.files(path = "data/Konrad Ecological Functions",
                      full.names = TRUE,
                      recursive = TRUE)
 kImages <- kfiles[grepl(pattern = "w001000.adf", x = kfiles)]
+
 ### read in object of interest for cropping 
 
 #fn1
-k_fn1<-terra::rast(kImages[grepl(pattern = "fn1", x = kImages)])%>%
-  terra::project(coreCRS)
-writeRaster(k_fn1, filename = "data/preppedValidataionData/fn1.tif" )
+library(tictoc)
+# ESPG number pulled from the st_crs(ifiFloodPlain)
+k_fn1<-raster::raster(kImages[grepl(pattern = "fn1", x = kImages)])
+writeRaster(k_fn1, filename = "data/preppedValidataionData/fn1.tif",overwrite = TRUE )
 
 #fn2
-k_fn2<-terra::rast(kImages[grepl(pattern = "fn2", x = kImages)])%>%
-  terra::project(coreCRS)
-writeRaster(k_fn1, filename = "data/preppedValidataionData/fn2.tif" )
+k_fn2<-raster::raster(kImages[grepl(pattern = "fn2", x = kImages)])
+writeRaster(k_fn2, filename = "data/preppedValidataionData/fn2.tif",overwrite = TRUE )
 
 #fn3
-k_fn3<-terra::rast(kImages[grepl(pattern = "fn3", x = kImages)])%>%
-  terra::project(coreCRS)
-writeRaster(k_fn1, filename = "data/preppedValidataionData/fn3.tif" )
+k_fn3<-raster::raster(kImages[grepl(pattern = "fn3", x = kImages)])
+writeRaster(k_fn1, filename = "data/preppedValidataionData/fn3.tif",overwrite = TRUE )
 
 #fn4
-k_fn4<-terra::rast(kImages[grepl(pattern = "fn4", x = kImages)])%>%
-  terra::project(coreCRS)
-writeRaster(k_fn1, filename = "data/preppedValidataionData/fn4.tif" )
-
-
-# comparison of flood plain areas  ----------------------------------------
-## this will be a difficult thing to do
-
-# process rasters per flood plain feature ---------------------------------
-
-## crop other spatial features 
-ifi_fpCrop <- ifiFloodPlain %>% sf::st_crop(ext(k_fn1))
-sf::st_write(obj = ifi_fpCrop, dsn = "data/preppedValidataionData/ifi_fpCrop.geojson")
-ifi_rCrop <- ifiResults %>% sf::st_crop(ext(k_fn1))
-sf::st_write(obj = ifi_rCrop, dsn = "data/preppedValidataionData/ifiResults.geojson", delete_dsn = TRUE)
+k_fn4<-raster::raster(kImages[grepl(pattern = "fn4", x = kImages)])
+writeRaster(k_fn1, filename = "data/preppedValidataionData/fn4.tif",overwrite = TRUE)
 
 
 
-# generate storage data frame  --------------------------------------------
-cellAreas <- data.frame(matrix(nrow = nrow(ifi_rCrop), ncol = 11))
-names(cellAreas) <- c("huc12_id", "calculatedArea", "measuredArea", "fp_area", "nCells", "3","2","1","0","-1","-2")
-
-# assign values from the ifi data
-cellAreas$huc12_id <- ifi_rCrop$huc12
-cellAreas$calculatedArea <- sf::st_area(ifi_rCrop)
-cellAreas$measuredArea <- ifi_rCrop$IFI_geomea
-cellAreas$fp_area <- ifi_rCrop$FP_Areakm2
-
-# processing function to calculate input values   ----------------------------------------------------
-for(i in seq_along(ifi_rCrop$huc12)){
-  # grad the huc features 
-  huc <- vect(ifi_rCrop[i,])
-  # crop 
-  tic()
-  crop <- terra::crop(k_fn1, huc )%>%
-    terra::mask(huc)
-  toc()
-  # pull out values 
-  vals <- values(crop)[,1]
-  cellAreas[i, "nCells"] <- length(vals)
-  # gather all unique values 
-  u_vals <- unique(vals)
-  # condition to test for the presents of any unique measure 
-  condition <- u_vals %in% c(3,2,1,0,-1,-2)
-  # validate condition, are any of the values present. 
-  if(TRUE %in% condition){
-    print(i)
-    cellAreas[i, "3"] <- length(grep(pattern = "3", x = vals))
-    cellAreas[i, "2"] <- length(grep(pattern = "2", x = vals))
-    cellAreas[i, "1"] <- length(grep(pattern = "1", x = vals))
-    cellAreas[i, "0"] <- length(grep(pattern = "0", x = vals))
-    cellAreas[i, "-1"] <- length(grep(pattern = "-1", x = vals))
-    cellAreas[i, "-2"] <- length(grep(pattern = "-2", x = vals))
-  }
-}
-
-write.csv(x = cellAreas, file = paste0("outputs/cellAreaClassification_",Sys.Date(),".csv"))
--- 4_visualizeValidationSet.R
+++ 4_visualizeValidationSet.R
@@ -4,7 +4,8 @@
 # carverd@colostate.edu 
 ###
 
-pacman::p_load(sf,terra,dplyr)
+pacman::p_load(sf,terra,dplyr, plotly, tmap, readr)
+tmap::tmap_mode("view")
 
 # general method 
 ## determine the total number of huc12s with IFI but no Konrad score 
@@ -33,20 +34,105 @@ k_fn4 <- rast("data/preppedValidataionData/fn4.tif" )
 
 
 
+# generalized function for percent area -----------------------------------
 
+
+#' calculatePercentages
+#'
+#' @param df : hold content for the 
+#' @param hucSF : ifi_results -- huc area
+#' @param fpSF : ifi_fp -- flood plain feature 
+#' @param K_rast : konrad raster layer 
+#' @param K_index : vector of measures in the konrad values to included
+#'
+#' @return
+#' 
+calculatePercentages <- function(df, hucSF, fpSF, K_rast, K_index){
+  for(i in seq_along(hucSF$huc12)){
+    # grab the huc ID, used as an index for the reset of the analysis 
+    hucID <- hucSF[i, "huc12"] %>% st_drop_geometry() %>% pull()
+    # grab huc object and convert to a terra vect 
+    huc <- hucSF[i,] %>% vect()
+    # grab the flood plain feature
+    ifi_fp <- fpSF[grepl(pattern = hucID, x = fpSF$HUC12), ]
+    if(nrow(ifi_fp) == 1){
+      # crop to FP
+      crop_fp <- terra::crop(K_rast, ifi_fp)%>%
+        terra::mask(ifi_fp)
+      # test if any values are in the data
+      val_crop <- values(crop_fp)[,1]
+      u_vals_crop <- unique(val_crop)
+      condition_crop <- u_vals_crop %in% K_index
+      if(TRUE %in% condition_crop){
+        df$cell_In_IFI_FP[i] <- TRUE
+      }else{
+        df$cell_In_IFI_FP[i] <- FALSE
+      }
+    }
+    
+    # crop the konrad data to area of the huc
+    crop <- terra::crop(K_rast, huc )%>%
+      terra::mask(huc)
+    # pull out all values
+    vals <- values(crop)[,1]
+    # gather all unique values
+    u_vals <- unique(vals)
+    # condition to test for the presents of any unique measure
+    condition <- u_vals %in% K_index
+    # validate condition, are any of the values present.
+    if(TRUE %in% condition){
+      trueVals <- vals[!is.na(vals)]
+      df$k_totalCells[i] <- length(trueVals)
+      df$k_sum[i] <- length(trueVals[trueVals >= 1])
+    }else{
+      df$k_totalCells[i] <- 0
+      df$k_sum[i] <- NA
+    }
+    print(paste0("end", i))
+  }
+  return(df)
+}
+
+
+
+# template df for location; require name structure ------------------------
+
+df_template <- data.frame(matrix(nrow = nrow(ifi_results), ncol = 9))
+
+names(df_template) <- c("huc12_id", "ifi_measure_Flood","ifi_fp_AreaKM2", 
+                      "konrad_fp_area", "k_totalCells", "k_sum", 
+                      "k_percentArea", "floodPlainAreaRatio", "cell_In_IFI_FP")
+# assign values from the ifi data
+df_template$huc12_id <- ifi_results$huc12_num
+df_template$ifi_measure_Flood <- ifi_results$Floods
+df_template$ifi_fp_AreaKM2 <- ifi_results$FP_Areakm2
 
 # Flood reduction ---------------------------------------------------------
 ## IFI : Flood reduction
 ## konrad : FN1 --- sum(1,2,3)
 
-# generate storage data frame  --------------------------------------------
-cellAreas <- data.frame(matrix(nrow = nrow(ifi_results), ncol = 7))
-names(cellAreas) <- c("huc12_id", "ifi_measure_Flood","ifi_fp_AreaKM2", "konrad_fp_area", "k_totalCells", "k_sum_1_2_3", "k_percentArea")
+kn1 <- calculatePercentages(df = df_template,
+                            hucSF = ifi_results,
+                            fpSF = ifi_fp,
+                            K_rast = k_fn1,
+                            K_index = c(1,2,3))
 
-# assign values from the ifi data
-cellAreas$huc12_id <- ifi_results$huc12_num
-cellAreas$ifi_measure_Flood <- ifi_results$Floods
-cellAreas$ifi_fp_AreaKM2 <- ifi_results$FP_Areakm2
+kn1$k_percentArea <- (kn1$k_sum/kn1$k_totalCells)*100
+kn1$konrad_fp_area <- kn1$k_totalCells * 0.0001
+kn1$floodPlainAreaRatio <- kn1$ifi_fp_AreaKM2 / kn1$konrad_fp_area
+
+write_csv()
+#filter steps 
+
+
+fig_fn1 <- plot_ly(data = kn1, 
+               x = ~ifi_measure_Flood, 
+               y = ~k_percentArea,
+               color = ~floodPlainAreaRatio,
+               text = ~paste("huc12: ", huc12_id, "<br>",
+                             "flood plain area ratio:", floodPlainAreaRatio)
+)
+fig_fn1
 
 
 # processing function to calculate input values   ----------------------------------------------------
@@ -78,25 +164,101 @@ for(i in seq_along(ifi_results$huc12)){
     cellAreas$k_totalCells[i] <- 0 
     cellAreas$k_sum_1_2_3[i] <- NA
   }
-  cellAreas$k_percentArea[i] <- (cellAreas$k_sum_1_2_3[i]/cellAreas$k_totalCells[i])*100
-  
 }
+cellAreas$k_percentArea <- (cellAreas$k_sum_1_2_3/cellAreas$k_totalCells)*100
+cellAreas$konrad_fp_area <- cellAreas$k_totalCells * 0.0001
+cellAreas$floodPlainAreaRatio <- cellAreas$ifi_fp_AreaKM2 / cellAreas$konrad_fp_area
 
-write.csv(x = cellAreas, file = paste0("outputs/cellAreaClassification_",Sys.Date(),".csv"))
+
+
+fig_fn1 <- plot_ly(data = cellAreas, 
+               x = ~ifi_measure_Flood, 
+               y = ~k_percentArea,
+               color = ~floodPlainAreaRatio,
+               text = ~paste("huc12: ", huc12_id, "<br>",
+                             "flood plain area ratio:", floodPlainAreaRatio)
+              )
+fig
+
+## example image of the two features 
+j <- 770
+## 59 bad over lap and score  
+## 934 --- Great overlap with area and scores 
+## 696 --- good areas alignment but discrepency between score
+## 770 --- no overlap between floodplain classes 
+
+# grab the huc ID, used as an index for the reset of the analysis 
+hucID <- ifi_results[j, "huc12"] %>% st_drop_geometry() %>% pull()
+# grab huc object and convert to a terra vect 
+huc <- ifi_results[j,] %>% vect()
+
+# crop the konrad data to area of the huc
+crop <- terra::crop(k_fn1, huc )%>%
+  terra::mask(huc)
+#ifi flood plain 
+
+f1 <- ifi_fp %>% filter(HUC12 == hucID)
+
+tm_shape(ifi_results[j,], "huc12")+
+  tm_borders()+
+  tm_shape(f1, "ifi_floodplain")+
+    tm_borders()+
+    tm_shape(crop, "konrad_floodplain")+
+      tm_raster()+
+  tm_layout(legend.position = c("right", "top"), title= paste(f1$HUC12, ": index", j))
+
+View()
+write.csv(x = cellAreas, file = paste0("outputs/validationFloods_fn1",Sys.Date(),".csv"))
 
 
 
-# Sediment regulation -----------------------------------------------------
+  # Sediment regulation -----------------------------------------------------
 ## IFI : Sediment regulation
 ## konrad fn2 --- sum(1,2,3,4)
+
+kn2 <- calculatePercentages(df = df_template,
+                            hucSF = ifi_results,
+                            fpSF = ifi_fp,
+                            K_rast = k_fn2,
+                            K_index = c(1,2,3,4))
+
+kn2$k_percentArea <- (kn2$k_sum/kn2$k_totalCells)*100
+kn2$konrad_fp_area <- kn2$k_totalCells * 0.0001
+kn2$floodPlainAreaRatio <- kn2$ifi_fp_AreaKM2 / kn2$konrad_fp_area
+
+
+
+
 
 
 # organic and solute regulation -------------------------------------------
 ## IFI : Organic & solute regulation
 ## konrad fn3 --- sum(>0)
 
+kn3 <- calculatePercentages(df = df_template,
+                            hucSF = ifi_results,
+                            fpSF = ifi_fp,
+                            K_rast = k_fn3,
+                            K_index = c(1,2,3))
+
+kn3$k_percentArea <- (kn3$k_sum/kn3$k_totalCells)*100
+kn3$konrad_fp_area <- kn3$k_totalCells * 0.0001
+kn3$floodPlainAreaRatio <- kn3$ifi_fp_AreaKM2 / kn3$konrad_fp_area
+
 
 # habitat provisioning ----------------------------------------------------
 ## IFI : Habitat provisioning
 ## konrad fn4 --- sum(1,2)
 
+
+kn4 <- calculatePercentages(df = df_template,
+                            hucSF = ifi_results,
+                            fpSF = ifi_fp,
+                            K_rast = k_fn4,
+                            K_index = c(1,2,3,4)) 
+                            
+                            kn4$k_percentArea <- (kn4$k_sum/kn4$k_totalCells)*100
+                            kn4$konrad_fp_area <- kn4$k_totalCells * 0.0001
+                            kn4$floodPlainAreaRatio <- kn4$ifi_fp_AreaKM2 / kn4$konrad_fp_area
+                            
+                            
